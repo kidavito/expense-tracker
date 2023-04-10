dcfea0e3951467ae707cd4400b190eb87c1fc11e
kidavito <kidavito.user@gmail.com>
2023-04-10 20:25:28 +0700

    add expense item component and expense date component inside it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
+  const expenses = [
+    // Array, to store multiple elements (items).
+    {
+      id: "e1",
+      title: "Expense 1",
+      amount: 100.0,
+      date: new Date(2023, 3, 11),
+    }, // [0]
+    {
+      id: "e2",
+      title: "Expense 2",
+      amount: 200.0,
+      date: new Date(2023, 3, 11),
+    }, // [1]
+    {
+      id: "e3",
+      title: "Expense 3",
+      amount: 300.0,
+      date: new Date(2023, 3, 11),
+    }, // [2]
+    // Note: The month and day are using array format from "Date()" which starts counting from 0, so "2023, 3, 11" means "2023, April 10".
+  ];
   return (
     <div>
-      <header>
-        <p>Text</p>
-      </header>
+      <h1>Expense Tracker!</h1>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
     </div>
   );
 }
