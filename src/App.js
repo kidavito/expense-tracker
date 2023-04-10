a4e0d5637b1e64149417208655a889837941de5e
kidavito <kidavito.user@gmail.com>
2023-04-11 00:53:42 +0700

    add another way to write the HTML using React create element

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 // Tree Structure: Expenses.js > ExpenseItem.js > ExpenseDate.js:
 import Expenses from "./components/Expenses";
 
@@ -24,6 +25,16 @@ function App() {
     }, // [2]
     // Note: The month and day are using array format from "Date()" which starts counting from 0, so "2023, 3, 11" means "2023, April 10".
   ];
+
+  // Render HTML using React object (without JSX):
+  // return React.createElement(
+  //   "div",
+  //   {},
+  //   React.createElement("h2", {}, "Expense Tracker!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
+  // Render HTML using JSX:
   return (
     <div>
       <h1>Expense Tracker!</h1>
