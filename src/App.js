a8c3e38f931b10cfd776ee3fb92cbf6ed2b8c253
kidavito <kidavito.user@gmail.com>
2023-04-10 20:41:21 +0700

    Expenses component created and passing props into it

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import ExpenseItem from "./components/ExpenseItem";
+// Tree Structure: Expenses.js > ExpenseItem.js > ExpenseDate.js:
+import Expenses from "./components/Expenses";
 
 function App() {
   const expenses = [
@@ -26,21 +27,7 @@ function App() {
   return (
     <div>
       <h1>Expense Tracker!</h1>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
+      <Expenses items={expenses} />
     </div>
   );
 }
