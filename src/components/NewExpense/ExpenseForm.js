26cee0365ac692edbcd146af2bf90dedc35ebee9
kidavito <kidavito.user@gmail.com>
2023-04-11 18:49:40 +0700

    initial useState configured

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,21 +1,118 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 function ExpenseForm() {
+  // Individual "useState":
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  // Grouped "useState" using "object":
+  //   const [userInput, setUserInput] = useState({
+  //     enteredTitle: "",
+  //     enteredAmount: "",
+  //     enteredDate: "",
+  //   });
+
+  const titleChangeHandler = (event) => {
+    // [WAY-1] Updating the value of individual "useState":
+    setEnteredTitle(event.target.value);
+
+    // [WAY-2] Updating the value of grouped "useState" (Not the best practice):
+    // setUserInput({
+    //   ...userInput, // Obtaining all object properties first to prevent loss of (un-mentioned) properties.
+    //   enteredTitle: event.target.value, // Updating the "enteredTitle" object property.
+    // });
+
+    // [WAY-3] Updating the value of grouped "useState" (Recommended):
+    // setUserInput((prevState) => {
+    //   // Using "prevState" will guarantee that you'll get the latest state.
+    //   return { ...prevState, enteredTitle: event.target.value };
+    // });
+  };
+
+  const amountChangeHandler = (event) => {
+    // [WAY-1] Updating the value of individual "useState":
+    setEnteredAmount(event.target.value);
+
+    // [WAY-2] Updating the value of grouped "useState" (Not the best practice):
+    // setUserInput({
+    //   ...userInput, // Obtaining all object properties first to prevent loss of (un-mentioned) properties.
+    //   enteredAmount: event.target.value, // Updating the "enteredTitle" object property.
+    // });
+
+    // [WAY-3] Updating the value of grouped "useState" (Recommended):
+    // setUserInput((prevState) => {
+    //   // Using "prevState" will guarantee that you'll get the latest state.
+    //   return { ...prevState, enteredAmount: event.target.value };
+    // });
+  };
+
+  const dateChangeHandler = (event) => {
+    // [WAY-1] Updating the value of individual "useState":
+    setEnteredDate(event.target.value);
+
+    // [WAY-2] Updating the value of grouped "useState" (Not the best practice):
+    // setUserInput({
+    //   ...userInput, // Obtaining all object properties first to prevent loss of (un-mentioned) properties.
+    //   enteredDate: event.target.value, // Updating the "enteredTitle" object property.
+    // });
+
+    // [WAY-3] Updating the value of grouped "useState" (Recommended):
+    // setUserInput((prevState) => {
+    //   // Using "prevState" will guarantee that you'll get the latest state.
+    //   return { ...prevState, enteredDate: event.target.value };
+    // });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault(); // Preventing the page from reloading automatically after submit.
+
+    // Set "expenseData" object properties:
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    console.log(expenseData);
+
+    // Reset/empty the form fields after submitting:
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={enteredAmount}
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            value={enteredDate}
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
@@ -24,5 +121,7 @@ function ExpenseForm() {
     </form>
   );
 }
+
+// Note: using "value" and "onChange" in the input form and binding it with "useState" is called "Two-Way Binding".
 
 export default ExpenseForm;
